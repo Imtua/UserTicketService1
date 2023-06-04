cc9e0cff9735a78fb70d9206dd390a2a22c4df07
Imtua <itsololevelling@gmail.com>
2023-06-04 13:39:50 +0300

    Добавьте файлы проекта.

---
+++ UserTicketService.Tests(XUnit)/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace UserTicketService.Tests_XUnit_
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestVoid()
+        {
+            Assert.True(100 == 100);
+        }
+    }
+}
